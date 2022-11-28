7c4b73f3778c1473b8f4fc6d2c07cc54f6adc8d1
Aladin <67257097+Aladin-Saleh@users.noreply.github.com>
2022-11-28 09:25:47 +0100

    gh

--- client/src/JSONHandler.java
+++ client/src/JSONHandler.java
@@ -16,13 +16,16 @@ public class JSONHandler {
     // Lecture d'une grappe de données au format JSON
     public Map<String, Object> readJSON(String json)
     {
-        try {
+        try 
+        {
             Object obj              = mapper.readValue(json, Object.class);
             Map<String, Object> map = (Map<String, Object>) obj;
 
             System.out.println("Lecture de la grappe de données au format JSON effectuée avec succès.");
             return map;
-        } catch (Exception e) {
+        } 
+        catch (Exception e) 
+        {
             System.err.println("Erreur lors de la lecture de la grappe de données au format JSON. " + e.getMessage());
             System.out.println(e.getMessage());
             e.printStackTrace();
@@ -31,7 +34,8 @@ public class JSONHandler {
     }
 
 
-    public ObjectMapper getMapper() {
+    public ObjectMapper getMapper() 
+    {
         return this.mapper;
     }
 
