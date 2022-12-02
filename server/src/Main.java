835280a5739decdb09febabc696f6929bf57ccaf
Argail-GISCLARD <83653820+Argail-GISCLARD@users.noreply.github.com>
2022-12-02 11:59:13 +0100

    Ajout affichage du labyrinthe
    - Ajout de la fonctionnalité pour afficher le labyrinthe

--- server/src/Main.java
+++ server/src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Labyrinth labyrinth = new Labyrinth(10, 10);
+        labyrinth.Display();
     }
 }
