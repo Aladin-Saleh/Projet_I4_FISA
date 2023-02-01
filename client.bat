74ecef40c65d1b8cb9d4532976e106d50abb22b2
Argail-GISCLARD <argail.gisclard@outlook.fr>
2023-02-01 19:01:00 +0100

    Fix des .bat de compilation

--- client.bat
+++ client.bat
@@ -1,6 +1,6 @@
-cd .\client\
-javac -cp build/jackson-annotations-2.14.0-rc2.jar;build/jackson-core-2.14.0-rc2.jar;build/jackson-databind-2.14.0-rc2.jar -d ./build/ ./src/*.java
-cd ./build/
+cd client\
+javac -cp build/jackson-annotations-2.14.0-rc2.jar;build/jackson-core-2.14.0-rc2.jar;build/jackson-databind-2.14.0-rc2.jar -d build/ ./src/*.java
+cd build/
 del Main.jar
 jar cvfm Main.jar manifest.txt *.class
 java -jar Main.jar
